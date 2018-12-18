9c20f734c8be0c635e134217e94f0eaf54d85cef
lvdesign <mailbox@lvdesign.com.fr>
2018-12-18 17:35:57 +0100

    test

---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+var del = require('del');
+var browserSync = require('browser-sync');
+
+
+const server = browserSync.create();
+
+// BrowserSync
+function serve(done) {
+ server.init({
+    server: {
+      baseDir: "./docs"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function reload(done) {
+ server.reload();
+  done();
+}
+
+
+var paths = {
+  styles: {
+    src: ['scss/bootstrap.css', 'scss/main.scss'],
+    dest: 'docs/css'
+  },
+  scripts:{
+    src: ['js/vendor/jquery-3.3.1.min.js','js/vendor/jquery.easing.min.js','js/vendor/scrollreveal.min.js','js/vendor/bootstrap.js' ,'js/main.js'],
+    dest: 'docs/js'
+  },
+  images:{
+    src:'img/**/*.{png,jpg,svg}',
+    dest: 'docs/img'
+  }
+
+
+};
+
+function clean(){
+  return del(['./docs']);
+}
+
+function images() {
+  return gulp.src(paths.images.src)
+  .pipe(imagemin([
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+    })
+  ]))
+  .pipe(gulp.dest(paths.images.dest));
+}
+
+function copy(){
+  return gulp.src('*.{html,txt,xml,webmanifest,png}')
+  .pipe(gulp.dest('docs'));
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))   // ici on utilise gulp-sass
+        .pipe( rename ({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest));
+        //.pipe(server.stream()); // browser
+}
+
+////.pipe(rename({extname: '.min.js'}))
+function scripts(){
+  return gulp
+  .src(paths.scripts.src, {
+    sourcemaps:true
+  })
+  .pipe(uglify())
+  .pipe(concat('main.min.js'))
+  .pipe(gulp.dest(paths.scripts.dest));
+}
+
+//
+function watch(){
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts );
+  gulp.watch(paths.images.src, images );
+  //gulp.watch(paths.styles.src, gulp.series(styles,reload));
+}
+
+//
+exports.copy = copy;
+exports.clean= clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watch = watch;
+exports.server = server;
+exports.reload = reload;
+//
+//var build = gulp.series(clean, gulp.parallel(styles));
+var build = gulp.parallel(styles,scripts);
+
+var live = gulp.series( clean, build, images, copy, serve, watch);
+
+gulp.task(build);
+gulp.task(live);
+gulp.task('default', build);
+
+gulp.task('v', live);
